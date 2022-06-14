cc27565ac1821cbb652550c5455ab8f797abcc67
dev-torugo <victorhacmo@gmail.com>
2022-06-14 17:34:22 -0300

    button implementation

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,8 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = 0 // private to certificate its a strict local variable
     var body: some View {
-        NavigationView {
+        Button("TapCount: \(tapCount)") {
+            tapCount += 1
+        }
+        // --------------- NAVIGATION VIEW BLOCK OF CODE ---------------
+       /* NavigationView {
             Form { // we can add up to 10 "things" insede that form structure. If we want to add more than 10 we need to use Group.
                 Section { // section split things "fisically"
                     Text("Hello, world!")
@@ -17,7 +22,7 @@ struct ContentView: View {
             }
             .navigationTitle("WeSplit")
             .navigationBarTitleDisplayMode(.inline)
-        }
+        }*/
     }
 }
 
