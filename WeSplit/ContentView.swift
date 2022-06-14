5815e718f83a7788283484e5c9ad961920fce47b
dev-torugo <victorhacmo@gmail.com>
2022-06-14 17:45:36 -0300

    binding state to user interface

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,11 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var tapCount = 0 // private to certificate its a strict local variable
+//    @State private var tapCount = 0 // private to certificate its a strict local variable
+    @State private var name = ""
     var body: some View {
-        Button("TapCount: \(tapCount)") {
-            tapCount += 1
+        
+        Form {
+            TextField("Enter your name", text: $name) // $ -> two-way binding: we bind the text field so that it shows the value of our property, but we also bind it so that any changes to the text field also update the property.
+            Text("Your name is: \(name)")
+
         }
+        
+        // --------------- BUTTON IMPLEMENTATION B.O.C -----------------
+       /* Button("TapCount: \(tapCount)") {
+            tapCount += 1
+        } */
+        
         // --------------- NAVIGATION VIEW BLOCK OF CODE ---------------
        /* NavigationView {
             Form { // we can add up to 10 "things" insede that form structure. If we want to add more than 10 we need to use Group.
