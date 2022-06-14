0270ceffecae8c2949ca7561e830af30052c9064
dev-torugo <victorhacmo@gmail.com>
2022-06-14 17:21:52 -0300

    NavigationView

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Form { // we can add up to 10 "things" insede that form structure. If we want to add more than 10 we need to use Group.
-            Section { // section split things "fisically"
-                Text("Hello, world!")
+        NavigationView {
+            Form { // we can add up to 10 "things" insede that form structure. If we want to add more than 10 we need to use Group.
+                Section { // section split things "fisically"
+                    Text("Hello, world!")
+                }
             }
-            Section {
-                Text("Hello, water!")
-            }
+            .navigationTitle("WeSplit")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
